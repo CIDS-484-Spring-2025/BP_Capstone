3ae603181cc66e2de1fdf9f88a0283cabca9ad2b
bprestholdt <bradenprestholdt@gmail.com>
2025-02-26 14:52:20 -0600

    initial controller class outlines to handle HTTP requests, Service class outline to handle business logic/calculatipons

--- backend/src/main/java/com/BP/setlistaggregator/repositories/SetlistRepository.java
+++ backend/src/main/java/com/BP/setlistaggregator/repositories/SetlistRepository.java
@@ -3,11 +3,12 @@ package com.BP.setlistaggregator.repositories;
 import com.BP.setlistaggregator.model.Setlist;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
-
+//data access class directly interacts with postgreSQL database
+//uses Spring Data JPA to perform queries, CRUD methods
 //interface for CRUD on Setlist entities
 public interface SetlistRepository extends JpaRepository<Setlist, Long> {
 
     //method to retrieve all setlists belonging to an artist using their artist ID
-    List<Setlist> findByArtistID(Long artistID);
+    List<Setlist> findByArtistName(String artist);
 }
 
